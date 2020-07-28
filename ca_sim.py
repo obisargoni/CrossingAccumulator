1a8480322c1534de634e76d6c83b04a9396fa489
Obi Sargoni <othompsarg@hotmail.com>
2020-07-28 10:33:29 +0100

    Processing batch run data

--- ca_sim.py
+++ ca_sim.py
@@ -51,21 +51,26 @@ batch_run = BatchRunner(CrossingModel,
                         model_reporters = {"CrossingChoice":model_crossing_choices})
 batch_run.run_all()
 
-'''
-model = CrossingModel(	ped_origin = ped_start_location, ped_destination = road_length, road_length = road_length, road_width = road_width, vehicle_flow = 1, 
-						alpha = 1.2, gamma = 0.9, ped_speed = ped_walking_speed, lam_range = range(1,4), r_range = np.linspace(0.9,1.1,3, endpoint=True), 
-						a_rate_range = [1], n_peds = 50)
-while model.running:
-	model.step()
+batch_data = batch_run.get_model_vars_dataframe()
 
-# Get the data
-crossing_types = model.datacollector.get_agent_vars_dataframe().unstack()
+# Column titles are wrong, need to rename
+batch_data_columns = list(variable_params.keys()) + ['Run', 'CrossingChoice'] + list(fixed_params.keys())
+batch_data.columns = batch_data_columns
 
-# Find out proportion using different crossing types
-def filter_none_types(col):
-	col = col.dropna()
-	col.index = np.arange(len(col))
-	return col
-crossing_proportions = crossing_types.apply(filter_none_types).iloc[0]
-print(crossing_proportions.value_counts(dropna=False))
-'''
+batch_data['CrossingChoice'] = batch_data['CrossingChoice'].fillna("none")
+batch_data['BatchRun'] = batch_data['Run'].map(lambda r: r // 50)
+batch_data.drop('Run', axis=1, inplace=True)
+
+param_columns = [c for c in batch_data.columns if c not in ['Run','CrossingChoice']]
+
+# Group by param columns and calculate aggregate crossing proportions
+def agg_crossing_choice(df, col = 'CrossingChoice'):
+	counts = df[col].value_counts().to_dict()
+
+	for k,v in counts.items():
+		df[k] = v
+	df.drop(col, axis=1, inplace=True)
+	return df.drop_duplicates()
+
+agg_batch_data = batch_data.groupby('BatchRun').apply(agg_crossing_choice)
+
